3f18b1a2bd19e25b559f3ce0618c741fdf10baca
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-04 09:32:30 -0300

    add - ex.3

--- Ex_3.php
+++ Ex_3.php
@@ -1,6 +1,13 @@
 <?php
 
-    function mascararCPF()
+    function mascararCPF($cpf)
     {
-        
+        $cpf_mascarado = str_repeat("*", strlen($cpf) - 4) . substr($cpf, -4);
+        return $cpf_mascarado;
     }
+
+    $cpf = "09812387645"
+    $resultado = mascara_cpf($cpf);
+
+    echo "O CPF é: $cpf <br>";
+    echo "O CPF mascarado: $resultado <br>";
