ba6aa8c19a4ea3384a631994dcf62acc6635e544
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-06 09:13:03 -0300

    add - fiz ex 5

---
+++ Ex_5.php
@@ -0,0 +1,31 @@
+<?php
+
+function analisar_texto($texto);
+
+    $caracteres = str_word_count($texto);
+    $palavras = strlen($texto);
+    $vogais = 0;
+    $consoantes = 0;
+
+    for ($i=0; $i<strlen($texto); $i++) {
+        $letra = strtolower($texto[$i]);
+
+        if ($letra == "a" || $letra == "e" || $letra == "i" || $letra == "o" || $letra == "u") {
+            $vogais++;
+        } 
+        else if (ctype_alpha($letra)) {
+            $consoantes++;
+        }
+
+    }
+
+    echo "A quantidade de palavras é: " . $palavras . "<br>";
+    echo "A quantidade de caracteres é: " . $caracteres . "<br>";
+    echo "A quantidade de vogais é: " . $vogais . "<br>";
+    echo "A quantidade de consoantes é: " . $consoantes . "<br>";
+
+    $texto = "Exercício PHP"
+
+    echo "O texto é: " . $texto . "<br>";
+
+    analisar_texto($texto):
