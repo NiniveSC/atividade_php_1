859a0c6e5fa10b48c33d2bd21edda4ceff82e507
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-08 09:26:08 -0300

    add - fiz ex. 7

---
+++ Ex_7.php
@@ -0,0 +1,25 @@
+<?php
+
+function calcular_desconto($valor, $desconto){
+    if ($valor > 1000){
+        $desconto = 30;
+    } else if ($valor > 500){
+        $desconto = 20;
+    } else if ($valor > 100){
+        $desconto = 10;
+    } else {
+        $desconto = 0;
+    }
+
+    $valor_desconto = ($valor * $desconto)/ 100;
+    $valor_final = $valor - $valor_desconto;
+
+    return "O valor original é: R$ $valor <br>";
+    return "O desconto aplicado é: $desconto% <br>";
+    return "O valor com desconto é: R$ $valor_final <br>"
+
+}
+
+    $valor = 650;
+
+    echo calcular_desconto($valor, 0);
