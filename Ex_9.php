0f8b8ea68aeeeb4676e8d748c7a9293c9793f664
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-12 10:59:21 -0300

    add - fiz ex. 9

---
+++ Ex_9.php
@@ -0,0 +1,41 @@
+<?php
+
+function analisar_numeros($numero){
+    if($numero % 2 == 0){
+        $par_impar = "par";
+    } else {
+        $par_impar = "ímpar";
+    }
+
+    $primo = "Primo";
+
+    if($numero < 2){
+        $primo = "Não é primo";
+    }else{
+        for($i = 2; $i <= sqrt($numero); $i++){
+            if($numero % $i == 0){
+                $primo = "Não é primo";
+                break;
+            }
+        }
+    }
+
+    $soma = 0;
+
+    for($i = 1; $i < $numero; $i++){
+        if($numero % $i == 0){
+            $soma += $i;
+        }
+    }
+
+    if($soma == $numero){
+        $perfeito = "O número é perfeito";
+    } else {
+        $perfeito = "O número não é perfeito"
+    }
+
+    $numero = 59;
+
+    echo analisar_numeros($numero);
+
+}
