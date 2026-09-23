93f3354d8043ace100195021cb6f1fd3598ef455
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-09-23 08:06:56 -0300

    add - fiz o ex.12

---
+++ Ex_12.php
@@ -0,0 +1,38 @@
+<?php
+function analisar_produtos ($produtos, $pesquisa){
+
+    $maior_preco = max($produtos);
+    $menor_preco = min($produtos);
+    $media_preco = array_sum($produtos) / count($produtos);
+
+    $produto_mais_caro = array_search($maior_preco, $produtos);
+    $produto_mais_barato = array_search($menor_preco, $produtos);
+
+    $resultado = "<br>Produto mais caro: R$ " . $maior_preco;
+    $resultado = "<br>Produto mais barato: R$ " . $menor_preco;
+    $resultado = "<br>Média de preço dos produtos: R$ " . $media_preco;
+
+    if(array_key_exists($pesquisa, $produtos))
+        $resultado .= "<br>Produto informado: R$ " . $produtos[$pesquisa];
+    }else{
+        $resultado .= "<br>Produto informado não encontrado";
+    }
+    return $resultado;
+}
+
+    $produtos = [
+        "Pão" => 15.00,
+        "Queijo" => 7.50,
+        "Presunto" => 13.90
+        "Margarina" => 10.90
+    ];
+
+    $pesquisa = "Pão";
+
+    echo "Produtos disponíveis: <br>";
+    foreach ($produtos as $nome => $preco) {
+        echo "$nome: R$ $preco <br>";
+    }
+
+    echo "<br>Produtos informados pelo usuário: $pesquisa <br>";
+    echo analisar_produtos ($produtos, $pesquisa);
